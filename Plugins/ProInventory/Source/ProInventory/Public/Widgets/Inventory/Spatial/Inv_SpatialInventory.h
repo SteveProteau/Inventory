57e86247d9f78e3d469a3a62c736cfb4ab182eca
SteveProteau <111454303+SteveProteau@users.noreply.github.com>
2025-06-29 14:31:19 -0700

    Switcher buttons

--- Plugins/ProInventory/Source/ProInventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Plugins/ProInventory/Source/ProInventory/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -6,6 +6,7 @@
 #include "Widgets/Inventory/InventoryBase/Inv_InventoryBase.h"
 #include "Inv_SpatialInventory.generated.h"
 
+class UButton;
 class UInv_InventoryGrid;
 class UWidgetSwitcher;
 
@@ -16,6 +17,8 @@ UCLASS()
 class PROINVENTORY_API UInv_SpatialInventory : public UInv_InventoryBase
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeOnInitialized() override;
 
 private:
 	UPROPERTY(meta = (BindWidget))
@@ -29,4 +32,25 @@ private:
 
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UInv_InventoryGrid> Grid_Craftables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Equippables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Consumables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> Button_Craftables;
+
+	UFUNCTION()
+	void ShowEquippables();
+
+	UFUNCTION()
+	void ShowConsumables();
+
+	UFUNCTION()
+	void ShowCraftables();
+	
+	void DisableButton(UButton* Button);
+	void SetActiveGrid(UInv_InventoryGrid* Grid, UButton* Button);
 };

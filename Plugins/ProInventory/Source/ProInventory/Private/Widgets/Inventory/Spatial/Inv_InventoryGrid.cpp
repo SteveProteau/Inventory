2d706f52a61ac3bdfed590ba30ff0769f640561d
SteveProteau <111454303+SteveProteau@users.noreply.github.com>
2025-06-30 11:32:49 -0700

    Grid Slots

--- Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -2,3 +2,41 @@
 
 
 #include "Widgets/Inventory/Spatial/Inv_InventoryGrid.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Widgets/Inventory/GridSlots/Inv_GridSlot.h"
+#include "Widgets/Utils/Inv_WidgetUtils.h"
+
+void UInv_InventoryGrid::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	ConstructGrid();
+}
+
+void UInv_InventoryGrid::ConstructGrid()
+{
+	// Reserve space in array to avoid resizing each time we add an element
+	GridSlots.Reserve(Rows * Columns);
+
+	for (int32 j = 0; j < Rows; ++j)
+	{
+		for (int32 i = 0; i < Columns; ++i)
+		{
+			UInv_GridSlot* GridSlot = CreateWidget<UInv_GridSlot>(this, GridSlotClass);
+			CanvasPanel->AddChild(GridSlot);
+
+			const FIntPoint TilePosition(i, j);
+			GridSlot->SetTileIndex(UInv_WidgetUtils::GetIndexFromPosition(TilePosition, Columns));
+
+			// Position the slot in canvas
+			UCanvasPanelSlot* GridCPS = UWidgetLayoutLibrary::SlotAsCanvasSlot(GridSlot);
+			GridCPS->SetSize(FVector2D(TileSize));
+			GridCPS->SetPosition(TilePosition * TileSize);
+
+			GridSlots.Add(GridSlot);
+		}
+	}
+}

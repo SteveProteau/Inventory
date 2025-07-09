66025007cbcd8be1e5140eb694e3c4af2c54caef
SteveProteau <111454303+SteveProteau@users.noreply.github.com>
2025-07-09 10:15:13 -0700

    Grid slot textures

--- Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -68,6 +68,7 @@ void UInv_InventoryGrid::AddItemToIndices(const FInv_SlotAvailabilityResult& Res
 	for (const auto& Availability : Result.SlotAvailabilities)
 	{
 		AddItemAtIndex(NewItem, Availability.Index, Result.bStackable, Availability.AmountToFill);
+		UpdateGridSlots(NewItem, Availability.Index);
 	}
 }
 
@@ -99,6 +100,14 @@ void UInv_InventoryGrid::AddSlottedItemToCanvas(const int32 Index, const FInv_Gr
 	const FVector2D DrawPos = UInv_WidgetUtils::GetPositionFromIndex(Index, Columns) * TileSize;
 	const FVector2D DrawPosWithPadding = DrawPos + FVector2D(GridFragment->GetGridPadding());
 	CanvasSlot->SetPosition(DrawPosWithPadding);
+}
+
+void UInv_InventoryGrid::UpdateGridSlots(UInv_InventoryItem* NewItem, const int32 Index)
+{
+	check(GridSlots.IsValidIndex(Index));
+
+	UInv_GridSlot* GridSlot = GridSlots[Index];
+	GridSlot->SetOccupiedTexture();
 }
 
 void UInv_InventoryGrid::ConstructGrid()

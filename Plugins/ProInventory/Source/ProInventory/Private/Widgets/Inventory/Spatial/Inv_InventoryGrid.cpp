7ba310bd2d519ae50e32c9a1a2441eea38d69723
SteveProteau <111454303+SteveProteau@users.noreply.github.com>
2025-07-12 11:48:27 -0700

    How many stacks to add

--- Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -45,7 +45,10 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemMa
 	const FInv_StackableFragment* StackableFragment = Manifest.GetFragmentOfType<FInv_StackableFragment>();
 	Result.bStackable = StackableFragment != nullptr;
 	
-		// If stackable - determine how many stacks to add.
+	// If stackable - determine how many stacks to add.
+	const int32 MaxStackSize = StackableFragment ? StackableFragment->GetMaxStackSize() : 1;
+	int32 AmountToFill = StackableFragment ? StackableFragment->GetStackCount() : 1;
+	
 	// For each Grid Slot:
 		// If we don't have anymore to fill, break out of the loop early.
 		// Is this index claimed yet?

5d97f9632843be77d5aca17c195d81ba57f5e0a4
SteveProteau <111454303+SteveProteau@users.noreply.github.com>
2025-07-15 11:21:03 -0700

    Slotted Item Clicked

--- Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/ProInventory/Source/ProInventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -308,6 +308,11 @@ void UInv_InventoryGrid::AddStacks(const FInv_SlotAvailabilityResult& Result)
 	}
 }
 
+void UInv_InventoryGrid::OnSlottedItemClicked(int32 GridIndex, const FPointerEvent& MouseEvent)
+{
+	
+}
+
 void UInv_InventoryGrid::ConstructGrid()
 {
 	// Reserve space in the array to avoid resizing each time we add an element
@@ -367,6 +372,7 @@ UInv_SlottedItem* UInv_InventoryGrid::CreateSlottedItem(UInv_InventoryItem* Item
 	SlottedItem->SetIsStackable(bStackable);
 	const int32 StackUpdateAmount = bStackable ? StackAmount : 0;
 	SlottedItem->UpdateStackCount(StackUpdateAmount);
+	SlottedItem->OnSlottedItemClicked.AddDynamic(this, &ThisClass::OnSlottedItemClicked);
 
 	return SlottedItem;
 }
